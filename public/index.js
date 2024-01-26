5ebbad0a8884506ed8766c5dcc06d35114747de0
Daniel S <dsantos@mileiq.com>
2024-01-25 20:08:53 -0400

    remove auth from the payload

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 function fetchUserProfile() {
   // if (sessionStorage.getItem("userName")) {
-    // console.error("User already authenticated");
-    // how to handle when the token expires?
-    // return;
+  // console.error("User already authenticated");
+  // how to handle when the token expires?
+  // return;
   // }
   const authToken = sessionStorage.getItem("authToken");
 
@@ -27,7 +27,7 @@ function fetchUserProfile() {
       response.json();
     })
     .then((data) => {
-      console.log("User Info:", data);
+      // console.log("User Info:", data);
       // save the user's name to sessionStorage
       sessionStorage.setItem("userName", data.name);
       sessionStorage.setItem("userEmail", data.email);
@@ -127,57 +127,60 @@ function displaySubscriptions(subscriptions) {
     iconElement.style.display =
       sub.contentDetails.totalItemCount >= 10000 ? "unset" : "none";
     const unsubscribeElement = document.createElement("button");
-      unsubscribeElement.textContent = sub.id;
-      unsubscribeElement.dataset.subscriptionId = sub.id;
-      unsubscribeElement.onclick = function () {
-        const subId = this.dataset.subscriptionId;
-        const authToken = sessionStorage.getItem("authToken");
-        unsubscribe(authToken, subId);
-      }
-      listItem.appendChild(unsubscribeElement);
+    unsubscribeElement.textContent = sub.id;
+    unsubscribeElement.dataset.subscriptionId = sub.id;
+    unsubscribeElement.onclick = function () {
+      const subId = this.dataset.subscriptionId;
+      // const authToken = sessionStorage.getItem("authToken");
+      // unsubscribe(authToken, subId);
+      unsubscribe(subId);
+    };
+    listItem.appendChild(unsubscribeElement);
     list.appendChild(listItem);
   });
 }
 
-function unsubscribe(auth, id) {
-    console.info("inside unsubscribe")
-    console.log(id, auth)
-  fetch("https://api.codein.ca/unsubscribe", {
+function unsubscribe(id) {
+  console.info("inside unsubscribe");
+  // console.log(id, auth);
+  try {
+    fetch("https://api.codein.ca/unsubscribe", {
       method: "POST",
       headers: {
         //Authorization: `Bearer ${authToken}`,
         "Content-Type": "application/json",
       },
-// body must be unpacked in the destination.
-	  // the content from the fetch is a json object already. body content is a string
-	  body: JSON.stringify({auth, id})
-      
-})
+      // body must be deserialized/unpacked in the destination.
+      // the body content is a string
+      // body: JSON.stringify({ auth, id }),
+      body: JSON.stringify({ id }),
+    })
       .then((response) => {
         if (!response.ok) {
           throw new Error(JSON.stringify(response));
         }
-        return response.json();                                                                                                                            72     })
+        return response.json();
+      })
       .then((data) => {
-        console.log("all good with unsubscription");
-          document.getElementById(id).style.display = "none"
-
-          return "all good"
+        // console.log("all good with unsubscription");
+        document.getElementById(id).style.display = "none";
+        return "all good";
       })
       .catch((error) => {
-        console.error("Error fetching subscriptions:", error);
+        console.error("Error FETCH subscriptions:", error);
       });
+  } catch (error) {
+    console.error("Error TRY/CATCH fetching subscriptions:", error);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  
+document.addEventListener("DOMContentLoaded", function () {
   if (sessionStorage.getItem("authToken")) {
-//    fetchUserProfile();
+    //    fetchUserProfile();
     fetchSubscriptions();
     document.getElementById("logout").style.display = "block";
   } else {
     document.getElementById("load-more").style.display = "none";
     document.getElementById("logout").style.display = "none";
   }
-  
 });
